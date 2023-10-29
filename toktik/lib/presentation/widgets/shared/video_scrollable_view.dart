5bf5a93003eb46d7eec886c2f28c5b8a9ffce6c9
Chalchiuhcuauhtli <ricardo6582@gmail.com>
2023-10-28 23:06:41 -0600

    feat: add funcionality to PageView, and add a background for the videos

--- toktik/lib/presentation/widgets/shared/video_scrollable_view.dart
+++ toktik/lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
+import 'package:toktik/presentation/widgets/video/fullscreen_player.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -8,9 +10,41 @@ class VideoScrollableView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
+    return PageView.builder(
+      // This prop changes the page to vertical and changes the behiavor.
       scrollDirection: Axis.vertical,
+      // This prop change the behiavor the end of page like bounce the sceen.
       physics: const BouncingScrollPhysics(),
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final videoPost = videos[index];
+
+        return Stack(
+          children: [
+            //Video Player + gradiant
+            SizedBox.expand(
+              child: FullScreenPlayer(
+                caption: videoPost.caption,
+                videoUrl: videoPost.videoUrl,
+              ),
+            ),
+            // Bottons
+            Positioned(
+                bottom: 40, right: 20, child: VideoButtons(video: videoPost))
+          ],
+        );
+      },
+    );
+  }
+}
+
+// Notes:
+/*
+  this case render all children on memory for this example only have 7 items,
+  but imagine having 100, or 1000, however only need the prev, current and the next
+
+  For this reason exist PageView.builder, build on demand
+
       children: [
         Container(
           color: Colors.red,
@@ -34,6 +68,4 @@ class VideoScrollableView extends StatelessWidget {
           color: Colors.deepPurple,
         ),
       ],
-    );
-  }
-}
+*/
