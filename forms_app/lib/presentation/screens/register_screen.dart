a098b4598137b1a2f872580cddad48031a1b7286
Chalchiuhcuauhtli <rarroyo@arkusnexus.com>
2024-01-25 22:03:14 -0600

    feat: add validation to form

--- forms_app/lib/presentation/screens/register_screen.dart
+++ forms_app/lib/presentation/screens/register_screen.dart
@@ -38,38 +38,78 @@ class _RegisterView extends StatelessWidget {
   }
 }
 
-class _RegisterForm extends StatelessWidget {
+class _RegisterForm extends StatefulWidget {
   const _RegisterForm();
 
   @override
+  State<_RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<_RegisterForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String username = '';
+  String email = '';
+  String password = '';
+  @override
   Widget build(BuildContext context) {
     return Form(
+        key: _formKey,
         child: Column(
-      children: [
-        const CustomTextFormField(
-          label: 'User Name',
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        const CustomTextFormField(
-          label: 'Email',
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        const CustomTextFormField(
-          label: 'Password',
-          obscureText: true,
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        FilledButton.tonalIcon(
-            onPressed: () {},
-            icon: const Icon(Icons.save),
-            label: const Text('Create User')),
-      ],
-    ));
+          children: [
+            CustomTextFormField(
+              label: 'User Name',
+              onChanged: (value) => username = value,
+              validator: (value) {
+                if (value == null || value.isEmpty) return 'Field is required';
+                if (value.trim().isEmpty) return 'Field is required';
+                if (value.length < 8) return 'More of 8 characters';
+                return null;
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            CustomTextFormField(
+              label: 'Email',
+              onChanged: (value) => email = value,
+              validator: (value) {
+                if (value == null || value.isEmpty) return 'Field is required';
+                if (value.trim().isEmpty) return 'Field is required';
+                final emailRegExp = RegExp(
+                  r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+                );
+
+                if (!emailRegExp.hasMatch(value)) return 'email invalid';
+
+                return null;
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            CustomTextFormField(
+              label: 'Password',
+              obscureText: true,
+              onChanged: (value) => password = value,
+              validator: (value) {
+                if (value == null || value.isEmpty) return 'Field is required';
+                if (value.trim().isEmpty) return 'Field is required';
+                if (value.length < 8) return 'More of 8 characters';
+                return null;
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            FilledButton.tonalIcon(
+                onPressed: () {
+                  final isValid = _formKey.currentState!.validate();
+                  if (!isValid) return;
+                  print('$username, $email, $password');
+                },
+                icon: const Icon(Icons.save),
+                label: const Text('Create User')),
+          ],
+        ));
   }
 }
