4992db0d80d35edaa781d4772ce6beb59b2490f4
Chalchiuhcuauhtli <rarroyo@arkusnexus.com>
2024-12-02 13:35:15 -0600

    feat: add google_fonts and update app theme

--- teslo_app/lib/config/theme/app_theme.dart
+++ teslo_app/lib/config/theme/app_theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 const colorSeed = Color(0xff424CB8);
 const scaffoldBackgroundColor = Color(0xFFF8F7F7);
@@ -11,28 +12,28 @@ class AppTheme {
       colorSchemeSeed: colorSeed,
 
       ///* Texts
-      // textTheme: TextTheme(
-      //     titleLarge: GoogleFonts.montserratAlternates()
-      //         .copyWith(fontSize: 40, fontWeight: FontWeight.bold),
-      //     titleMedium: GoogleFonts.montserratAlternates()
-      //         .copyWith(fontSize: 30, fontWeight: FontWeight.bold),
-      //     titleSmall:
-      //         GoogleFonts.montserratAlternates().copyWith(fontSize: 20)),
+      textTheme: TextTheme(
+          titleLarge: GoogleFonts.montserratAlternates()
+              .copyWith(fontSize: 40, fontWeight: FontWeight.bold),
+          titleMedium: GoogleFonts.montserratAlternates()
+              .copyWith(fontSize: 30, fontWeight: FontWeight.bold),
+          titleSmall:
+              GoogleFonts.montserratAlternates().copyWith(fontSize: 20)),
 
       ///* Scaffold Background Color
       scaffoldBackgroundColor: scaffoldBackgroundColor,
 
       ///* Buttons
-      // filledButtonTheme: FilledButtonThemeData(
-      //     style: ButtonStyle(
-      //         textStyle: WidgetStatePropertyAll(
-      //             GoogleFonts.montserratAlternates()
-      //                 .copyWith(fontWeight: FontWeight.w700)))),
+      filledButtonTheme: FilledButtonThemeData(
+          style: ButtonStyle(
+              textStyle: WidgetStatePropertyAll(
+                  GoogleFonts.montserratAlternates()
+                      .copyWith(fontWeight: FontWeight.w700)))),
 
       ///* AppBar
-      appBarTheme: const AppBarTheme(
+      appBarTheme: AppBarTheme(
         color: scaffoldBackgroundColor,
-        // titleTextStyle: GoogleFonts.montserratAlternates().copyWith(
-        //     fontSize: 25, fontWeight: FontWeight.bold, color: Colors.black),
+        titleTextStyle: GoogleFonts.montserratAlternates().copyWith(
+            fontSize: 25, fontWeight: FontWeight.bold, color: Colors.black),
       ));
 }
