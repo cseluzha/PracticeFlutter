94f54b88729e2d224ca3a14da1a967723858a701
Chalchiuhcuauhtli <rarroyo@arkusnexus.com>
2024-12-02 21:40:20 -0600

    feat: upload images form gallery or camera to backend

--- teslo_app/lib/features/products/infrastructure/datasources/products_datasource_impl.dart
+++ teslo_app/lib/features/products/infrastructure/datasources/products_datasource_impl.dart
@@ -12,6 +12,36 @@ class ProductsDatasourceImpl extends ProductsDatasource {
             baseUrl: Environment.apiUrl,
             headers: {'Authorization': 'Bearer $accessToken'}));
 
+  Future<String> _uploadFile(String path) async {
+    try {
+      final fileName = path.split('/').last;
+      final formData = FormData.fromMap({
+        'file': await MultipartFile.fromFile(path, filename: fileName),
+      });
+      final response = await dio.post('/files/product', data: formData);
+      return response.data['image'];
+    } catch (e) {
+      throw Exception();
+    }
+  }
+
+  Future<List<String>> _uploadPhotos(List<String> photos) async {
+    final photosToUpload =
+        photos.where((photo) => photo.startsWith('/')).toList();
+    final photosToIgnore =
+        photos.where((photo) => !photo.startsWith('/')).toList();
+
+    //Create a list of Future<String> from the photosToUpload list
+    final List<Future<String>> uploadJob =
+        photosToUpload.map(_uploadFile).toList();
+    final newImages = await Future.wait(uploadJob);
+
+    return [
+      ...photosToIgnore,
+      ...newImages,
+    ];
+  }
+
   @override
   Future<Product> createUpdateProduct(Map<String, dynamic> productLike) async {
     try {
@@ -21,6 +51,8 @@ class ProductsDatasourceImpl extends ProductsDatasource {
           productId == null ? '/products' : '/products/$productId';
 
       productLike.remove('id');
+      productLike['images'] = await _uploadPhotos(productLike['images']);
+
       final response = await dio.request(url,
           data: productLike, options: Options(method: method));
       final product = ProductMapper.jsonToEntity(response.data);
